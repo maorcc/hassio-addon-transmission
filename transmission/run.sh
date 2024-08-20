053e9addf56ed9af43268d0a99c60bf43eb29756
maor <maor@Maors-MacBook-Pro.local>
2024-08-21 02:28:28 +0300

    minor change to trigger a build on Canary branch

--- transmission/run.sh
+++ transmission/run.sh
@@ -16,6 +16,7 @@ EOF
     bashio::log.info 'Setting initial whitelist to allow access to web UI from local network.'
 fi
 
+
 bashio::log.info 'Starting Transmission Daemon'
 /usr/bin/transmission-daemon -g "$config_dir" -f
 bashio::log.info 'Transmission Daemon stopped.'
